6dd9cb1c4f89f5fb829001d93bc796f5c994c5ff
Ethan Flaharty <ethanflahartybusiness@gmail.com>
2026-01-29 22:33:05 -0600

    CH2 L4 LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,4 +19,18 @@ class HTMLNode:
         
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("value is none")
+        if self.tag is None:
+            return f'{self.value}'
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
     
