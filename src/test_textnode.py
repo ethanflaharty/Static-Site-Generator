3a0601366d8561562ae3ebcceb66edc9da284b94
Ethan Flaharty <ethanflahartybusiness@gmail.com>
2026-01-29 23:29:45 -0600

    7.2

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -15,17 +15,17 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
     def test_eq_false2(self):
-        node = TextNode("This is a text node", TextType.TEXT)
-        node2 = TextNode("This is a text node2", TextType.TEXT)
+        node = TextNode("This is a text node", TextType.text)
+        node2 = TextNode("This is a text node2", TextType.text)
         self.assertNotEqual(node, node2)
 
     def test_eq_url(self):
-        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
-        node2 = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        node = TextNode("This is a text node", TextType.text, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.text, "https://www.boot.dev")
         self.assertEqual(node, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        node = TextNode("This is a text node", TextType.text, "https://www.boot.dev")
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
