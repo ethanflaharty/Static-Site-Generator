1f483d9ef696ac9738b3a5c7de1007c160c1cba5
Ethan Flaharty <ethanflahartybusiness@gmail.com>
2026-02-04 14:28:43 -0600

    16. CH5 L2 Generate Page

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -29,7 +29,7 @@ class LeafNode(HTMLNode):
             raise ValueError("value is none")
         if self.tag is None:
             return f'{self.value}'
-        return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
